ef08f209e596bcc47fabee3978faeb3eebf043b5
Jurnell Cockhren <jurnell@sophicware.com>
2016-11-02 19:31:54 -0700

    Finished assignment up to 'Create an Angular Driven Form'

---
+++ BasicsOfForms/Controllers/BasicsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicsOfForms.Controllers
+{
+    public class BasicsController : Controller
+    {
+        // GET: Basics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            int i = 0;
+            return View();
+        }
+
+        public ActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Razor(FormCollection form)
+        {
+            int i = 0;
+            return View();
+        }
+
+        public ActionResult Angular()
+        {
+            return View();
+        }
+    }
+}
